192692df9f0e079520253475cb6d689f43ee6f54
Noah Crosby <noah.b.crosby@gmail.com>
2025-10-21 16:31:17 -0500

    Adds work so far on a ball racer, and general track. We need to improve and understand how to use the movement here to create tracks and interest.

---
+++ scripts/ball.gd
@@ -0,0 +1,44 @@
+extends RigidBody2D
+
+@export var speed = 100
+@export var maxSpeed = 400
+var canMove = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	contact_monitor = true	
+	max_contacts_reported = 15
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	
+	var vertMove: float = Input.get_axis("up", "down")
+	var horizMove: float = Input.get_axis("left", "right")
+	if canMove && (vertMove != 0 || horizMove != 0) :		
+		if vertMove == 0:
+			if randf() > .5:
+				vertMove = -0.5
+			else :
+				vertMove = 0.5
+		
+		if linear_velocity.length() > maxSpeed:
+			print("too fast!")
+			return
+		
+		var impulseVect = Vector2(horizMove, vertMove)
+		impulseVect = impulseVect.normalized()
+		print("moving ", speed," and ", impulseVect)
+		apply_impulse(impulseVect * speed)
+		#apply_impulse(Vector2(impulseVect.y * speed,impulseVect.x * (speed/2)))
+		canMove = false
+	pass
+
+
+func _on_body_entered(body: Node) -> void:
+	print("colliding")
+	canMove = true
+	pass # Replace with function body.
